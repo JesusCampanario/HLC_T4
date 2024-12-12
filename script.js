eb500d83ec5501f1d2ab4f3ee6c4020683f0982b
JesusCampanario <jesus.campanariosanchez@iesvalleinclan.es>
2024-12-12 09:35:37 +0100

    primera versión

---
+++ script.js
@@ -0,0 +1,25 @@
+switch (tema) {
+    case claro:
+        input.classname = "oscuro"
+        textarea.className = "oscuro"
+        break;
+
+    case oscuro:
+        
+        break;
+
+    case azul:
+        
+        break;
+
+    case rosa:
+        
+        break;
+
+    case verde:
+        
+        break;
+
+    default:
+        break;
+}
